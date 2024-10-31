9ec65821902970c806ac4f32b915f57536864873
tomcotter7 <thomascotter00@gmail.com>
2024-10-31 09:11:43 +0000

    Added aliases for zsh

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+# aliases
+
+alias venv='function _venv() {
+    source $1/bin/activate
+}; _venv'
+
+alias pdffer='function _pdffer() {
+    local tmp_file
+    tmp_file=$(mktemp).pdf && \
+    curl -s "$1" > "$tmp_file" && \
+    zathura "$tmp_file" && \
+    rm "$tmp_file"
+}; _pdffer'
