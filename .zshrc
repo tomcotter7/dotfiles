438e489fd167f2b629cda358744b10fc4d8856f9
Tom Cotter <thomascotter00@gmail.com>
2025-04-24 09:33:23 +0100

    new aliases, tmux options (for image.nvim)

--- .zshrc
+++ .zshrc
@@ -121,6 +121,6 @@ fi
 export PATH="$PATH:/opt/nvim/"
 
 # uncomment for language specifc stuff
-# . "$HOME/.cargo/env"
+. "$HOME/.cargo/env"
 # export PATH="$PATH:/home/tcotter/.modular/bin"
 # export PATH=$PATH:/usr/local/go/bin
--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,16 @@
 # aliases
 
+alias mkvenv='function _mkvenv() {
+  python$1 -m venv .venv
+}; _mkvenv'
+
 alias venv='function _venv() {
     source $1/bin/activate
 }; _venv'
+
+alias rmvenv='function _rmvenv() {
+  rm -r $1
+}; _rmvenv'
 
 alias pdffer='function _pdffer() {
     local tmp_file
